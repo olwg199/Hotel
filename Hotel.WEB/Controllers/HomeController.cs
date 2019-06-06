588c90b135b585d75f791a613b7059fcabb10565
Oleg Yastrebov <olwg199@gmail.com>
2019-06-06 14:26:08 +0300

    Added main view

--- Hotel.WEB/Controllers/HomeController.cs
+++ Hotel.WEB/Controllers/HomeController.cs
@@ -4,22 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
 
 namespace Hotel.WEB.Controllers
 {
     public class HomeController : Controller
     {
-        private IConvenienceService _service;
+        private IRoomTypeService _service;
+        private IMapper _mapper;
 
-        public HomeController(IConvenienceService service)
+        public HomeController(IRoomTypeService service, IMapper mapper)
         {
             _service = service;
+            _mapper = mapper;
         }
 
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(_service.GetAll());
         }
     }
 }
---
+++ Hotel.WEB/Controllers/ReservarionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel.WEB.Controllers
+{
+    public class ReservarionController : Controller
+    {
+        // GET: Reservarion/Create/Id
+        public ActionResult Create(int id)
+        {
+            return View();
+        }
+    }
+}
